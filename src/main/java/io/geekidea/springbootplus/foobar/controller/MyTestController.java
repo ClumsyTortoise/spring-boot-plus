8743b4cd756e98530d24e5f52724cd3f8cc2baaa
git <git>
2019-11-04 21:51:56 +0800

    加注释

--- src/main/java/io/geekidea/springbootplus/foobar/controller/MyTestController.java
+++ src/main/java/io/geekidea/springbootplus/foobar/controller/MyTestController.java
@@ -1,23 +1,19 @@
 package io.geekidea.springbootplus.foobar.controller;
 
-import io.geekidea.springbootplus.foobar.entity.MyTest;
-import io.geekidea.springbootplus.foobar.service.MyTestService;
-import io.geekidea.springbootplus.foobar.param.MyTestQueryParam;
-import io.geekidea.springbootplus.foobar.vo.MyTestQueryVo;
 import io.geekidea.springbootplus.common.api.ApiResult;
 import io.geekidea.springbootplus.common.controller.BaseController;
+import io.geekidea.springbootplus.common.vo.PageInfo;
+import io.geekidea.springbootplus.foobar.entity.MyTest;
+import io.geekidea.springbootplus.foobar.param.MyTestQueryParam;
+import io.geekidea.springbootplus.foobar.service.MyTestService;
+import io.geekidea.springbootplus.foobar.vo.MyTestQueryVo;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-
 import javax.validation.Valid;
-
-import io.geekidea.springbootplus.common.vo.PageInfo;
-import io.geekidea.springbootplus.common.param.IdParam;
 
 /**
  * <pre>
